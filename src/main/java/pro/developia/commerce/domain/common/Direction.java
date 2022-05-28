2974b27018138fb22e2a607ff206ce20e787044e
developiaa <developiaa@gmail.com>
2022-05-28 21:30:30 +0900

    chore : Delete unused files

--- src/main/java/pro/developia/commerce/domain/common/Direction.java
+++
@@ -1,6 +0,0 @@
-package pro.developia.commerce.domain.common;
-
-public enum Direction {
-    DESC,
-    ASC;
-}
--- src/main/java/pro/developia/commerce/domain/common/Environment.java
+++
@@ -1,2 +0,0 @@
-package pro.developia.commerce.domain.common;public interface Environment {
-}
