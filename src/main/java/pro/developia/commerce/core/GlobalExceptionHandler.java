578325f5a7d70ea34c70c715c269de364b44b294
developiaa <developiaa@gmail.com>
2022-07-23 21:35:33 +0900

    feat : Modify exception handler(Valid exception)

--- src/main/java/pro/developia/commerce/core/GlobalExceptionHandler.java
+++ src/main/java/pro/developia/commerce/core/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package pro.developia.commerce.core;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<ApiResult<String>> handlerApiException(ApiException e) {
+    public ResponseEntity<ApiResult<String>> apiExceptionHandler(ApiException e) {
         Code code = e.getCode();
         String message = e.getMessage();
         HttpStatus httpStatus = e.getHttpStatus();
@@ -19,9 +20,16 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<ApiResult<String>> handleNullPointerException(final NullPointerException e) {
-        log.error("handleNullPointerException : {}", e.getMessage());
+    public ResponseEntity<ApiResult<String>> nullPointerExceptionHandler(final NullPointerException e) {
+        log.error("nullPointerExceptionHandler : {}", e.getMessage());
         return new ResponseEntity<>(ApiResult.fail(Code.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResult<String>> methodArgumentNotValidExceptionHandler(Exception e) {
+        log.error("methodArgumentNotValidExceptionHandler : {}", e.getMessage());
+        return new ResponseEntity<>(ApiResult.fail(Code.BAD_REQUEST,
+                ((MethodArgumentNotValidException) e).getAllErrors().get(0).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
 }
