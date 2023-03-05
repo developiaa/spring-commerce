48ae1946c4ec91282c5a7cf9a4fc0bbe4ddf1ac6
developiaa <developiaa@gmail.com>
2023-03-05 23:42:11 +0900

    feat: Add admin api module

---
+++ commerce-admin-api/src/main/java/pro/developia/admin/CommerceAdminApiApplication.java
@@ -0,0 +1,13 @@
+package pro.developia.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceAdminApiApplication.class, args);
+	}
+
+}
---
+++ commerce-admin-api/src/test/java/pro/developia/admin/CommerceAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.developia.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
