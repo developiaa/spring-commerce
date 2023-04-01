3fa9e659f59bb1f28a50fd82eb3a0ae87e42dfbf
developiaa <developiaa@gmail.com>
2023-04-01 23:11:12 +0900

    feat: Add order domain module

---
+++ order-domain/src/main/java/pro/developia/orderdomain/OrderDomainApplication.java
@@ -0,0 +1,13 @@
+package pro.developia.orderdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDomainApplication.class, args);
+    }
+
+}
---
+++ order-domain/src/test/java/pro/developia/orderdomain/OrderDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.developia.orderdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
